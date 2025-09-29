ac55134014eab38fbfdc598aa56642195a56732c
Maltide <maltid1998@gmail.com>
2025-09-29 19:40:46 +0300

    NextTask function implemented

--- pkg/store/next.go
+++ pkg/store/next.go
@@ -1,8 +1,31 @@
 package store
 
-import "github.com/Maltide/notification_bot_tg/pkg/types"
+import (
+	"fmt"
+
+	"github.com/Maltide/notification_bot_tg/pkg/types"
+)
 
 func (ms *MemoryStore) NextTask() (types.Task, error) {
-	// TODO найти и вернуть наиближайшую задачу
+	if len(ms.Data) == 0 {
+		return types.Task{}, fmt.Errorf("Нет заметок")
+	}
+
+	copied := false
+	nearTask := types.Task{}
+
+	for _, val := range ms.Data {
+		for _, task := range val {
+			if !copied {
+				nearTask = task
+				copied = true
+				continue
+			}
+			if task.DueAt.Before(nearTask.DueAt) {
+				nearTask = task
+			}
+		}
+	}
+
 	return types.Task{}, nil
 }
