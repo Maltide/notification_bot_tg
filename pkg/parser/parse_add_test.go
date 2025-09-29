044be2d7580df0bd94ddaee7f983c9436650ed79
Maltide <maltid1998@gmail.com>
2025-09-29 12:53:34 +0300

    Отредактировал тесты, изменил формат времени

--- pkg/parser/parse_add_test.go
+++ pkg/parser/parse_add_test.go
@@ -6,61 +6,62 @@ import (
 )
 
 func TestParseAdd(t *testing.T) {
-	now := time.Date(2025, 8, 31, 10, 0, 0, 0, time.UTC)
+
+	now := time.Date(2025, 9, 29, 11, 00, 0, 0, Moscow_current_time)
 
 	parser := NewTimeParser()
 
 	tests := []struct {
-		name, args, wantText string
-		wantDue              time.Time
-		wantErr              bool
+		name, userinput, wantText string
+		wantDue                   time.Time
+		wantErr                   bool
 	}{
 		{
-			name:     "ok simple",
-			args:     "10m drink water",
-			wantText: "drink water",
-			wantDue:  now.Add(10 * time.Minute),
-			wantErr:  false,
+			name:      "ok simple",
+			userinput: "29.09.25 11:10 drink water",
+			wantText:  "drink water",
+			wantDue:   time.Date(2025, 9, 29, 11, 10, 0, 0, Moscow_current_time),
+			wantErr:   false,
 		},
 		{
-			name:    "err empty string",
-			args:    "",
-			wantErr: true,
+			name:      "err empty string",
+			userinput: "",
+			wantErr:   true,
 		},
 		{
-			name:    "err only duration",
-			args:    "10m",
-			wantErr: true,
+			name:      "err only duration",
+			userinput: "29.09.25 11:00",
+			wantErr:   true,
 		},
 		{
-			name:    "err bad duration",
-			args:    "abc task",
-			wantErr: true,
+			name:      "err bad duration",
+			userinput: "abc task",
+			wantErr:   true,
 		},
 		{
-			name:    "err zero duration",
-			args:    "0m task",
-			wantErr: true,
+			name:      "err zero duration",
+			userinput: "00.00.00 00:00 task",
+			wantErr:   true,
 		},
 		{
-			name:     "day_check",
-			args:     "24h daycheck test",
-			wantText: "daycheck test",
-			wantDue:  now.Add(24 * time.Hour),
-			wantErr:  false,
+			name:      "day_check",
+			userinput: "30.09.25 11:10 daycheck test",
+			wantText:  "daycheck test",
+			wantDue:   time.Date(2025, 9, 30, 11, 10, 0, 0, Moscow_current_time),
+			wantErr:   false,
 		},
 		{
-			name:     "1m30s",
-			args:     "1m30s go home",
-			wantText: "go home",
-			wantDue:  now.Add(1*time.Minute + 30*time.Second),
-			wantErr:  false,
+			name:      "1m",
+			userinput: "29.09.25 11:01 go home",
+			wantText:  "go home",
+			wantDue:   time.Date(2025, 9, 29, 11, 01, 0, 0, Moscow_current_time),
+			wantErr:   false,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			due, text, err := parser.ParseAddTask(tt.args, now)
+			due, text, err := parser.ParseAddTask(tt.userinput, now)
 
 			if tt.wantErr {
 				if err == nil {
--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -6,25 +6,33 @@ import (
 	"time"
 )
 
+var Moscow_current_time, _ = time.LoadLocation("Europe/Moscow")
+
+const inputlayout = "02.01.06 15:04"
+
 func NewTimeParser() *TimeParser {
 	return &TimeParser{}
 }
 
-func (tp *TimeParser) ParseAddTask(userinput string) (due time.Time, text string, err error) {
+func (tp *TimeParser) ParseAddTask(userinput string, now time.Time) (due time.Time, text string, err error) {
 	if strings.TrimSpace(userinput) == "" {
 		return time.Time{}, "", fmt.Errorf("Заметка отсутствует")
 	}
 
 	shards := strings.SplitN(userinput, " ", 3)
 	if len(shards) < 3 {
-		return time.Time{}, "", fmt.Errorf("Нужен формат именно такой: DD.MM.YY HH:MM <text>")
+		return time.Time{}, "", fmt.Errorf("Нужен формат именно такой: 12.11.25 15:05 <Сходить в магазин>")
 	}
 
-	notif_time := shards[0] + " " + shards[1]
+	notif_time := strings.ReplaceAll(shards[0]+" "+shards[1], ",", "")
 
-	due, err = time.ParseInLocation(userinput, notif_time, time.Local)
+	due, err = time.ParseInLocation(inputlayout, notif_time, Moscow_current_time)
 	if err != nil {
 		return time.Time{}, "", fmt.Errorf("Неправильный формат даты или времени")
+	}
+
+	if due.Before(now) {
+		return time.Time{}, "", fmt.Errorf("Вы пытаетесь ввести прошедшее время")
 	}
 
 	text = shards[2]
